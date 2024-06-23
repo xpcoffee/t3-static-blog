32ecbad5c4b55a92bb180cc9b927f6accce6a5af
Emerick Bosch <xpc.dev@gmail.com>
2024-06-23 16:34:59 +0200

    Remove server
    This will be statically rendered.

---
+++ src/app/_components/placeholder.tsx
@@ -0,0 +1,3 @@
+export const PlaceHolder = () => {
+  return <div> Placholder </div>;
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 import Link from "next/link";
-
-import { CreatePost } from "~/app/_components/create-post";
-import { api } from "~/trpc/server";
+import { PlaceHolder } from "./_components/placeholder";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
@@ -35,31 +31,9 @@ export default async function Home() {
               deploy it.
             </div>
           </Link>
+          <PlaceHolder />
         </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
-        </div>
-
-        <CrudShowcase />
       </div>
     </main>
-  );
-}
-
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest();
-
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
   );
 }
