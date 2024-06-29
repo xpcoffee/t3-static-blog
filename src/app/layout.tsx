8dbf7f300dd72d1338d56e7087aca65524f61358
Emerick Bosch <xpc.dev@gmail.com>
2024-06-29 14:11:05 +0200

    Fix favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { GeistSans } from "geist/font/sans";
 export const metadata = {
   title: "Emerick Bosch",
   description: "Some of my thoughts, for sharing.",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  icons: [{ rel: "icon", url: "/xpcoffee-icon.svg" }],
 };
 
 export default function RootLayout({
